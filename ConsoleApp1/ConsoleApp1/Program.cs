c9add54e7310dc8fbe67dc08558a113ed2ac1c16
v.pchelintseva <rome.otpusk@gmail.com>
2026-06-15 20:38:01 +0200

    выполнено Викой

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 Console.WriteLine("Hi I'm Pasha. I publish two books");
 Console.WriteLine("Hello My Name is Kirilenko Iaroslav, I am QA");
+Console.WriteLine("Hello My Name is Viktoriya, I love raspberries and the smell of freshly cut grass");
