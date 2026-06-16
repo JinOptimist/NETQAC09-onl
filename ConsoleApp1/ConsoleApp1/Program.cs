2bd81b849b9fc21052614c5c2eddfcd340f512fc
Jorjetta <jorjettamkryan@gmail.com>
2026-06-16 14:56:30 +0500

    Added hello messages

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 Console.WriteLine("Hi I'm Pasha. I publish two books");
 Console.WriteLine("Hello My Name is Kirilenko Iaroslav, I am QA");
+Console.WriteLine("Hello My Name is Jorjetta, I am from Armenia");,
