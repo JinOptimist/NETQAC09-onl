1b18c511c7c93b49e0162d4cdd7ed1f557399ecc
unknown <ywallen213@gmail.com>
2026-06-18 11:33:34 +0200

    add_line_matov

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,3 +10,4 @@ Console.WriteLine("Hello My Name is Max, I love snowboarding");
 Console.WriteLine("Yo, my name's Andrey. I love swimming");
 Console.WriteLine("Hi I'm Irina. I live in Prague");
 Console.WriteLine("Hi I'm Irina2. I live in Prague");
+Console.WriteLine("Dobar dan! My name is Valerii and this is some info about me.");
