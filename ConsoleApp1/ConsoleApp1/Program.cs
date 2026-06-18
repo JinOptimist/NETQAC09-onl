1e967cd417599719243cb07efbaa46404cd1164e
gavririna <gavririna>
2026-06-18 08:48:52 +0200

    test2-46-task

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,3 +9,4 @@ Console.WriteLine("Hello My Name is A.Makhortov");
 Console.WriteLine("Hello My Name is Max, I love snowboarding");
 Console.WriteLine("Yo, my name's Andrey. I love swimming");
 Console.WriteLine("Hi I'm Irina. I live in Prague");
+Console.WriteLine("Hi I'm Irina2. I live in Prague");
