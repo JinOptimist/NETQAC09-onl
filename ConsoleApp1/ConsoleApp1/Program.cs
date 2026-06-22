e4db8c6dd327e12af7178fde54201bb3153d1fb9
Ilya <ilyaturgenev@gmail.com>
2026-06-22 10:02:51 +0300

    Ilya added lines for updating the game

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,60 +1,93 @@
 Console.WriteLine("The game Guess the number");
 
-var MAX_ATTEMPT = 3;
-var MAX_NUMBER = 100;
-var MIN_NUMBER = 1;
+Console.WriteLine("Enter MIN number");
+var MIN_NUMBER = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter MAX number");
+var MAX_NUMBER = int.Parse(Console.ReadLine());
+
+var MAX_ATTEMPT = (int)Math.Ceiling(Math.Log2(MAX_NUMBER - MIN_NUMBER + 1));
+
+Console.WriteLine("Who will choose magic number?");
+Console.WriteLine("1 - Human");
+Console.WriteLine("2 - Computer");
+
+var mode = int.Parse(Console.ReadLine());
 
 var userMagicNumber = 0;
-bool isNumber;
-do
+
+if (mode == 1)
 {
-    Console.WriteLine("User 1. Enter Magic number");
+    bool isNumber;
+    do
+    {
+        Console.WriteLine("User 1. Enter Magic number");
 
-    var userMagicNumberText = Console.ReadLine();
-    isNumber = int.TryParse(userMagicNumberText, out userMagicNumber);
+        var userMagicNumberText = Console.ReadLine();
+        isNumber = int.TryParse(userMagicNumberText, out userMagicNumber);
 
-    if (!isNumber)
-    {
-        Console.WriteLine("It's not a number");
-    }
-    else if (userMagicNumber > MAX_NUMBER)
-    {
-        Console.WriteLine($"Too big number. Must be less then {MAX_NUMBER}");
-    }
-    else if (userMagicNumber < MIN_NUMBER)
-    {
-        Console.WriteLine($"Too small number. Must be more then {MIN_NUMBER}");
-    }
-} while (!isNumber
-    || userMagicNumber < MIN_NUMBER
-    || userMagicNumber > MAX_NUMBER);
+        if (!isNumber)
+        {
+            Console.WriteLine("It's not a number");
+        }
+        else if (userMagicNumber > MAX_NUMBER)
+        {
+            Console.WriteLine($"Too big number. Must be less then {MAX_NUMBER}");
+        }
+        else if (userMagicNumber < MIN_NUMBER)
+        {
+            Console.WriteLine($"Too small number. Must be more then {MIN_NUMBER}");
+        }
+    } while (!isNumber
+        || userMagicNumber < MIN_NUMBER
+        || userMagicNumber > MAX_NUMBER);
+}
+else
+{
+    var random = new Random();
+    userMagicNumber = random.Next(MIN_NUMBER, MAX_NUMBER + 1);
+}
 
 Console.Clear();
+
 var attempt = 0;
 int guess;
 var isWin = false;
+
+var currentMinNumber = MIN_NUMBER;
+var currentMaxNumber = MAX_NUMBER;
+
 do
 {
-    attempt++;
-    //Console.WriteLine("User 2. Enter your guess. Attemmpt [" + attempt + " / " + MAX_ATTEMPT + "]");
-    Console.WriteLine($"User 2. Enter your guess. Attemmpt [{attempt} / {MAX_ATTEMPT}]");
+    Console.WriteLine($"Current range: [{currentMinNumber} - {currentMaxNumber}]");
+    Console.WriteLine($"User 2. Enter your guess. Attemmpt [{attempt + 1} / {MAX_ATTEMPT}]");
+
     var guessText = Console.ReadLine();
     guess = int.Parse(guessText);
+
+    if (guess < currentMinNumber || guess > currentMaxNumber)
+    {
+        Console.WriteLine("Number is outside current range. Attempt is not counted.");
+        continue;
+    }
+
+    attempt++;
 
     if (guess < userMagicNumber)
     {
         Console.WriteLine("Our number is bigger");
+        currentMinNumber = guess + 1;
     }
     else if (guess > userMagicNumber)
     {
         Console.WriteLine("Our number is less");
+        currentMaxNumber = guess - 1;
     }
     else if (guess == userMagicNumber)
     {
         isWin = true;
     }
 } while (!isWin && attempt < MAX_ATTEMPT);
-
 
 if (isWin)
 {
@@ -64,76 +97,3 @@ else
 {
     Console.WriteLine("Loooose");
 }
-
-//Console.WriteLine("Hi I'm Pasha. I publish two books");
-//Console.WriteLine("Hello My Name is Kirilenko Iaroslav, I am QA");
-//Console.WriteLine("Hello My Name is Anna Tyletskaya, blah-blah-blah");
-//Console.WriteLine("Hi, I'm Andrei. I play drums");
-//Console.WriteLine("Hello My Name is Kirilenko Iaroslav, I am QA");
-//Console.WriteLine("Hello My Name is Jorjetta, I am from Armenia");
-//Console.WriteLine("Hello My Name is Viktoriya, I love raspberries and the smell of freshly cut grass");
-//Console.WriteLine("Hola! Yo soy Jack. Vivo en Montenegro");
-//Console.WriteLine("Hello My Name is Mykola Andruk, I have a pug");
-//Console.WriteLine("Hello My Name is Andrei, I love playing poker");
-//Console.WriteLine("Hello My Name is A.Makhortov");
-//Console.WriteLine("Hello My Name is Max, I love snowboarding");
-//Console.WriteLine("Yo, my name's Andrey. I love swimming");
-//Console.WriteLine("Hi I'm Irina. I live in Prague");
-//Console.WriteLine("Hi I'm Irina2. I live in Prague");
-//Console.WriteLine("Hi, I'm Timur and i love to work every day");
-//Console.WriteLine("Hello My Name is Ekaterina, I love hiking");
-//Console.WriteLine("Dobar dan! My name is Valerii and this is some info about me.");
-
-
-//int age = 20;
-//char symbol = '1';
-//string name = "text";
-
-//bool isAdult = false;
-//bool condition1 = true;
-//bool condition2 = !isAdult; // not
-//bool condition3 = condition1 && condition2; // and
-//bool condition4 = condition3 || condition2; // or
-//bool condition5 = !condition2 && condition3 || condition4 && condition1;
-
-//var test = 123;
-//test = 1;
-//// test = "qwe";
-
-//var test2 = "Test";
-//// test2 = 123;
-//test2 = "qwe";
-
-//Console.WriteLine("Hi");
-//if (age > 60)
-//{
-//    Console.WriteLine("Go to vilage");
-//}
-//else if (age > 30)
-//{
-//    Console.WriteLine("Go to Work");
-//}
-//else if (age > 18 && condition2)
-//{
-//    if (condition4)
-//    {
-//        Console.WriteLine("Go to super univer");
-//    }
-//    Console.WriteLine("Go to univer 1");
-//}
-//else
-//{
-//    Console.WriteLine("Go to school 1");
-//    Console.WriteLine("Go to school 2");
-//}
-
-//Console.WriteLine("End");
-
-
-//var indexForNextWhile = 0;
-//while (indexForNextWhile < 10)
-//{
-//    indexForNextWhile = indexForNextWhile + 1;
-//    Console.WriteLine(indexForNextWhile);
-//}
-
