8c6a773aa786002619e22b3264ba6d8f1da76f1a
asheremet-lab <a.sheremet@evernode.tech>
2026-06-24 14:03:35 +0300

    Lesson 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-var lvou = new andriisheremet();
+using ConsoleApp1;
+
+var lvou = new Lvou();
 lvou.Do();
+
+
+//var userName = "Ivan";
+//var userAge = 20;
+//var isMan = true;
+
+//var userNameSecond = "Lera";
+//var userAgeSecond = 18;
+//var isManSecond = false;
+
+var ivan = new User();
+ivan.name = "Ivan";
+ivan.age = 20;
+ivan.isMan = true;
+
+var lera = new User();
+lera.name = "Lera";
+lera.age = 20;
+lera.isMan = false;
+
+
+Console.WriteLine($"{ivan.name} {ivan.age}");
