2c6ea03d5d03486575fa6361b054cb9a0a17e97a
ParfenovAV <parfenovandrej233@gmail.com>
2026-06-27 12:23:36 +0500

    HW_3  исправил замечания ментора

--- ConsoleApp1/ConsoleApp1/GameParfenov.cs
+++ ConsoleApp1/ConsoleApp1/GameParfenov.cs
@@ -3,11 +3,11 @@ using ConsoleApp1;
 
     class GameParfenov
 {
-    private int MAX_ATTEMPT = 10;
-    private int MIN_NUMBER;
-    private int MAX_NUMBER;
-    private int userMagicNumber;
-    private bool isWin;
+    private int _maxAttemp = 10;
+    private int _minNumber;
+    private int _maxNumber;
+    private int _userMagicNumber;
+    private bool _isWin;
 
     public void Play()
     {
@@ -27,29 +27,37 @@ using ConsoleApp1;
         {
             Console.WriteLine("Please enter the MIN_NUMBER");
             var MIN_NUMBER_Text = Console.ReadLine();
-            isNumberMin = int.TryParse(MIN_NUMBER_Text, out MIN_NUMBER);
+            isNumberMin = int.TryParse(MIN_NUMBER_Text, out _minNumber);
 
             if (!isNumberMin)
+            { 
                 Console.WriteLine("It's not a number");
-            else if (MIN_NUMBER < 0)
+            }
+            else if (_minNumber < 0)
+            {
                 Console.WriteLine("enter a number greater than 0");
+            }
 
-        } while (!isNumberMin || MIN_NUMBER < 0);
+        } while (!isNumberMin || _minNumber < 0);
 
         bool isNumberMax;
-        var minMaxNumber = MIN_NUMBER + 1;
+        var minMaxNumber = _minNumber + 1;
         do
         {
             Console.WriteLine($"Please enter the MAX_NUMBER > {minMaxNumber}");
             var MAX_NUMBER_Text = Console.ReadLine();
-            isNumberMax = int.TryParse(MAX_NUMBER_Text, out MAX_NUMBER);
+            isNumberMax = int.TryParse(MAX_NUMBER_Text, out _maxNumber);
 
             if (!isNumberMax)
-                Console.WriteLine("It's not a number");
-            else if (MAX_NUMBER <= minMaxNumber)
-                Console.WriteLine($"The number is too small. There must be more than {minMaxNumber}");
+            {
+                Console.WriteLine("It's not a number"); 
+            }
+            else if (_maxNumber <= minMaxNumber)
+            {
+                Console.WriteLine($"The number is too small. There must be more than {minMaxNumber}"); 
+            }
 
-        } while (!isNumberMax || MAX_NUMBER <= minMaxNumber);
+        } while (!isNumberMax || _maxNumber <= minMaxNumber);
     }
 
     private void ChooseGuesser()
@@ -71,21 +79,28 @@ using ConsoleApp1;
                 {
                     Console.WriteLine("User 1. Enter Magic number");
                     var userMagicNumberText = Console.ReadLine();
-                    isNumber = int.TryParse(userMagicNumberText, out userMagicNumber);
+                    isNumber = int.TryParse(userMagicNumberText, out _userMagicNumber);
 
                     if (!isNumber)
+                    { 
                         Console.WriteLine("It's not a number");
-                    else if (userMagicNumber > MAX_NUMBER)
-                        Console.WriteLine($"Too big number. Must be less then {MAX_NUMBER}");
-                    else if (userMagicNumber < MIN_NUMBER)
-                        Console.WriteLine($"Too small number. Must be more then {MIN_NUMBER}");
+                    }
+                        
+                    else if (_userMagicNumber > _maxNumber)
+                    {
+                        Console.WriteLine($"Too big number. Must be less then {_maxNumber}"); 
+                    }
+                    else if (_userMagicNumber < _minNumber)
+                    {
+                        Console.WriteLine($"Too small number. Must be more then {_minNumber}"); 
+                    }
 
-                } while (!isNumber || userMagicNumber < MIN_NUMBER || userMagicNumber > MAX_NUMBER);
+                } while (!isNumber || _userMagicNumber < _minNumber || _userMagicNumber > _maxNumber);
             }
             else if (enterFlow == 2)
             {
                 var random = new Random();
-                userMagicNumber = random.Next(MIN_NUMBER, MAX_NUMBER);
+                _userMagicNumber = random.Next(_minNumber, _maxNumber);
             }
 
         } while (enterFlow != 1 && enterFlow != 2);
@@ -96,29 +111,43 @@ using ConsoleApp1;
     {
         var attempt = 0;
         int guess;
-        isWin = false;
+        bool numberGuess;
+        _isWin = false;
         do
         {
             attempt++;
-            Console.WriteLine($"User 2. Enter your guess. Attemmpt [{attempt} / {MAX_ATTEMPT}]");
+            Console.WriteLine($"User 2. Enter your guess. Attemmpt [{attempt} / {_maxAttemp}]");
             var guessText = Console.ReadLine();
-            guess = int.Parse(guessText);
+            numberGuess = int.TryParse(guessText, out guess);
 
-            if (guess < userMagicNumber)
-                Console.WriteLine("Our number is bigger");
-            else if (guess > userMagicNumber)
-                Console.WriteLine("Our number is less");
-            else if (guess == userMagicNumber)
-                isWin = true;
-
-        } while (!isWin && attempt < MAX_ATTEMPT);
+            if (guess < _userMagicNumber)
+            {
+                Console.WriteLine("Our number is bigger"); 
+            }
+            else if (guess > _userMagicNumber)
+            {
+                Console.WriteLine("Our number is less"); 
+            }
+            else if (guess == _userMagicNumber)
+            {
+                _isWin = true; 
+            }
+            if (!numberGuess)
+            {
+                Console.WriteLine("It's not a number");
+            }
+        } while (!_isWin && attempt < _maxAttemp);
     }
 
     private void ShowResult()
     {
-        if (isWin)
-            Console.WriteLine("Right! Your are Win");
+        if (_isWin)
+        {
+            Console.WriteLine("Right! Your are Win"); 
+        }
         else
-            Console.WriteLine("Loooose");
+        {
+            Console.WriteLine("Loooose"); 
+        }
     }
 }
