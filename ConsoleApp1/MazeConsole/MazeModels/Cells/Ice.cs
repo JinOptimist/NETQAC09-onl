5bf442041a5f9dee01af71b06e9d2eeea28f525a
a.tyletskaya <a.tyletskaya@evernode.tech>
2026-07-01 17:49:29 +0200

    ICe

--- ConsoleApp1/MazeConsole/MazeModels/Cells/Ice.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/Ice.cs
@@ -10,40 +10,45 @@ namespace MazeConsole.MazeModels.Cells
 
         public override bool PlayerStepInMe(Player player)
         {
-            // где игрок был до того, как встал на лед
-            var previousX = player.X;
-            var previousY = player.Y;
-
-            // в каком направлении игрок двигается
-            var directionX = X - previousX;
-            var directionY = Y - previousY;
-
-            // определить, куда надо проскользить
-            var moveToX = directionX + X;
-            var moveToY = directionY + Y;
-
-           //найти эту ячейку, куда игрок будет скользить
-            var nextCell = MazeWhereIWasCreated.
-            Cells.
-            First(cell => cell.X == moveToX && cell.Y == moveToY); 
-
-            //логику ниже нужно будет перепридумать в зависимости от того, как будет развиваться игра дальше (синергия с другими ячейками и свойствами ячеек, проверить на выезд за пределы лабиринта...)
-            //наступабельная ли найденная ячейка
-            if (nextCell.PlayerStepInMe(player) == false) 
-            //если нет - останавливаемся на нашей ячейке льда
+            // если в инвентаре есть песок, то уменьшаем количество песка в инвентаре и меняем лед на грязь
+            if (player.Sand > 0)
             {
-                player.X = X;
-                player.Y = Y;
+                player.Sand--;
+
+                var dirtCell = new Dirt { X = this.X, Y = this.Y };
+                MazeWhereIWasCreated.Cells[MazeWhereIWasCreated.Cells.IndexOf(this)] = dirtCell;
+
+                return true;
             }
-            
-            else if (nextCell.PlayerStepInMe(player) == true)
-            //если да - проскальзываем на следующую ячейку
+
+            // 1. Откуда идет игрок
+            var directionX = X - player.X;
+            var directionY = Y - player.Y;
+
+            // 2. куда игрок должен скользить (на 1 клетку дальше льда)
+            var moveToX = X + directionX;
+            var moveToY = Y + directionY;
+
+            // 3. поиск ячейки , на которую игрок должен скользить
+            var nextCell = MazeWhereIWasCreated.Cells
+                .FirstOrDefault(cell => cell.X == moveToX && cell.Y == moveToY);
+
+            // 4. Проверяем: если ячейка за льдом существует и наступабельная
+            if (nextCell != null && nextCell.PlayerStepInMe(player))
             {
+                // скользим
                 player.X = moveToX;
                 player.Y = moveToY;
             }
-        
-            return true;
+            else
+            {
+                // если ненаступабельная - останавливаемся на льду
+                player.X = X;
+                player.Y = Y;
+            }
+
+            // уличная магия - запрещаем контроллеру двигать игрока, т.к. подвигали сами
+            return false;
         }
     }
 }
