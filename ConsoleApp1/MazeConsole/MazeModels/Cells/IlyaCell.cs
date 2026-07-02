f4ac06c118504a4a9213be655ef288b3919052ee
Ilya <ilyaturgenev@gmail.com>
2026-07-02 16:29:45 +0300

    Ilya added logic

--- ConsoleApp1/MazeConsole/MazeModels/Cells/IlyaCell.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/IlyaCell.cs
@@ -1,16 +1,21 @@
-using MazeConsole.MazeModels;
-
 namespace MazeConsole.MazeModels.Cells;
 
 public class IlyaCell : BaseCell
 {
-	public override char MySymbol => 'I';
+    public override char MySymbol => 'I';
 
-	public override bool PlayerStepInMe(Player player)
-	{
-		Console.WriteLine("You found the Ilya Cell! +3 coins.");
-		player.Coin += 3;
+    public override bool PlayerStepInMe(Player player)
+    {
+        Console.WriteLine("You found Ilya Cell!");
 
-		return true;
-	}
+        var random = new Random();
+        var coins = random.Next(1, 6);
+
+        player.Coin += coins;
+
+        Console.WriteLine($"You received {coins} coins.");
+        Console.WriteLine($"Now you have {player.Coin} coins.");
+
+        return true;
+    }
 }
