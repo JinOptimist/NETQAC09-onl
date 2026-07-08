2bf0e7219751d4b984c2383b7e5c5693ecbddc8f
Andrei Drozd <a.drozd@evernode.tech>
2026-07-08 09:53:51 +0300

    Added exception

--- ConsoleApp1/MazeConsole/MazeModels/Cells/HealthPotion.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/HealthPotion.cs
@@ -7,8 +7,14 @@ public class HealthPotion : BaseCell
 
     public override bool PlayerStepInMe(Player player)
     {
+        if (player.HealthPotion >= 1)
+        {
+            throw new InvalidOperationException($"Player already has max HealthPotions (1). Position: ({X}, {Y})");
+        }
+
         player.HealthPotion++;
         MazeWhereIWasCreated.ReplaceCellToGround(this);
+        
         return true;
     }
 }
