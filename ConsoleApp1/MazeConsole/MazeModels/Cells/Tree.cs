c0cd46460dfa18e858bf62ffaba20c4cb860aa43
v.chumilova <v.chumilava@evernode.tech>
2026-07-08 23:58:23 +0300

    Chumilava HW6

--- ConsoleApp1/MazeConsole/MazeModels/Cells/Tree.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/Tree.cs
@@ -1,13 +1,32 @@
 ﻿namespace MazeConsole.MazeModels.Cells;
+using MazeConsole.MazeExceptions;
 
 public class Tree : BaseCell
 {
     public int Durability { get; set; } = 5;
     public override char MySymbol => 'W';
-
     public override bool PlayerStepInMe(Player player)
     {
-        return false;
+        try
+        {
+            var toGos = MazeWhereIWasCreated.Cells.OfType<Ground>().ToList();
+
+            if (toGos.Any())
+            {
+                var randomGround = toGos[new Random().Next(toGos.Count)];
+
+                player.X = randomGround.X;
+                player.Y = randomGround.Y;
+                var logger = new FileLogger();
+                logger.AddLog($"Игрок пытался залезть на дерево, но упал на координаты ({randomGround.X}; {randomGround.Y})");
+            }
+
+            return false;
+        }
+        catch (Exception)
+        { 
+            return true;
+        }
     }
 }
 
