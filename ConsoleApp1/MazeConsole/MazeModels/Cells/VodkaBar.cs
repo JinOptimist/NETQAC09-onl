8bb4fdc7d26e718a9bbede8e7bb2adb211557f9d
jack <jackvelorock@gmail.com>
2026-07-09 18:01:33 +0200

    log

--- ConsoleApp1/MazeConsole/MazeModels/Cells/VodkaBar.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/VodkaBar.cs
@@ -1,30 +1,53 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using MazeConsole.MazeModels.Cells;
+
 namespace MazeConsole.MazeModels.Cells;
 
-/* VodkaBar - место, попав в которое игрок зависает на долгое время,
- а потом просыпается мордой в рандомной клетке земли и без денег
- */
 public class VodkaBar : BaseCell
 {
-    public override char MySymbol => '⚗'; // иконка на карте
+    public override char MySymbol => '⚗';
+
+    private void LogAction(string message)
+    {
+        try
+        {
+            File.AppendAllText("vodka.log", $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}{Environment.NewLine}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка записи в лог: {ex.Message}");
+        }
+    }
 
     public override bool PlayerStepInMe(Player player)
     {
-        Console.WriteLine("Игрок в баре 🍻🍻🍻"); //логируем
+        LogAction("Игрок вошел в VodkaBar.");
+        Console.WriteLine("Игрок в баре 🍻🍻🍻");
         Thread.Sleep(5000);
         player.Coin = 0;
 
         var grounds = MazeWhereIWasCreated.Cells.OfType<Ground>().ToList();
-    
+
         if (grounds.Any())
         {
             var randomGround = grounds[new Random().Next(grounds.Count)];
-        
             player.X = randomGround.X;
             player.Y = randomGround.Y;
+            LogAction($"Игрок проснулся на земле в [{player.X}, {player.Y}].");
         }
-        
+        else
+        {
+            // Случай, если земли на карте вообще нет
+            string errorMsg = "КРИТИЧЕСКАЯ ОШИБКА: Земля (Ground) не найдена на карте!";
+            LogAction(errorMsg);
+            throw new Exception(errorMsg); 
+        }
+
         Console.WriteLine("Упс... Вы пропили весь кэш и очнулись утром непонятно где...");
         Thread.Sleep(3000);
-        return false; 
+        return false;
     }
 }
