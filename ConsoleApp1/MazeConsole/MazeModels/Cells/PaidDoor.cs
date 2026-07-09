e3ce91c3cc8c742dc77532faa879897602335a41
Andrei <andreytishkin1@gmail.com>
2026-07-09 08:31:40 +0200

    6

--- ConsoleApp1/MazeConsole/MazeModels/Cells/PaidDoor.cs
+++ ConsoleApp1/MazeConsole/MazeModels/Cells/PaidDoor.cs
@@ -10,6 +10,9 @@ public class PaidDoor : BaseCell
 
         if (player.Coin < doorPrice)
         {
+            MazeWhereIWasCreated.LogMessages.Add("You need 2 coins to open this door");
+            //Console.WriteLine("You need 2 coins to open this door");
+            return false;
             throw new Exception(
                 $"PaidDoor error. Position: {GetMyPosition()}. " +
                 $"Need coins: {doorPrice}. Player coins: {player.Coin}. " +
@@ -18,7 +21,8 @@ public class PaidDoor : BaseCell
 
         player.Coin = player.Coin - doorPrice;
         MazeWhereIWasCreated.ReplaceCellToGround(this);
-        Console.WriteLine("You opened the paid door");
+        MazeWhereIWasCreated.LogMessages.Add("You opened the paid door");
+        //Console.WriteLine("You opened the paid door");
         return true;
     }
 }
