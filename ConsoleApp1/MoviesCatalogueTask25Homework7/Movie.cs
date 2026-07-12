d6374cfce534c41f6224656e64a78c078ec8de41
atyletskayaevernode <a.tyletskaya@evernode.tech>
2026-07-12 16:12:20 +0200

    ...

--- ConsoleApp1/MoviesCatalogueTask25Homework7/Movie.cs
+++ ConsoleApp1/MoviesCatalogueTask25Homework7/Movie.cs
@@ -6,13 +6,12 @@ namespace MoviesCatalogueTask25Homework7
 {
     public class Movie
     {
-        public int Id { get; set; }
+        public int Id { get; set; } //если все-таки будут силы сделать сохранение в базу данных, то Id будет нужен для идентификации фильма в базе
         public string Title { get; set; }
         public int Year { get; set; }
         public MovieGenreList Genre { get; set; }
         public int? Rating { get; set; }
 
-        public Movie() { }
 
         public Movie(string title, int year, MovieGenreList genre)
         {
