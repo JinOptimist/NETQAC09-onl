9d3c194ef2f9989c1817428d9362491e984171e7
Andrei Drozd <a.drozd@evernode.tech>
2026-07-13 09:46:07 +0300

    Number bag class

--- ConsoleApp1/Bingo/NumberBag.cs
+++ ConsoleApp1/Bingo/NumberBag.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Bingo
 {
-    internal class NumberBag
+    public class NumberBag
     {
+        private readonly List<int> _available;
+        private readonly Random _rnd = new Random();
+        public List<int> Drawn { get; } = new List<int>();
+
+        public NumberBag(int max = 75)
+        {
+            _available = Enumerable.Range(1, max).ToList();
+        }
+
+        public int? Draw()
+        {
+            if (_available.Count == 0) return null;
+            int idx = _rnd.Next(_available.Count);
+            int number = _available[idx];
+            _available.RemoveAt(idx);
+            Drawn.Add(number);
+            return number;
+        }
     }
 }
