b2bfb5cda6b4d08d89d6b685dc72b906fe4a58db
Andrei Drozd <a.drozd@evernode.tech>
2026-07-13 09:44:34 +0300

    Bingo Card

--- ConsoleApp1/Bingo/BingoCard.cs
+++ ConsoleApp1/Bingo/BingoCard.cs
@@ -4,7 +4,72 @@ using System.Text;
 
 namespace Bingo
 {
-    internal class BingoCard
+    public class BingoCard
     {
+        public int[,] Numbers { get; } = new int[5, 5];
+        public bool[,] Marked { get; } = new bool[5, 5];
+        private static readonly Random _rnd = new Random();
+
+        public BingoCard()
+        {
+            Generate();
+            Marked[2, 2] = true; // центр бесплатный
+        }
+
+        private void Generate()
+        {
+            // Колонки B(1-15) I(16-30) N(31-45) G(46-60) O(61-75)
+            for (int col = 0; col < 5; col++)
+            {
+                var range = Enumerable.Range(col * 15 + 1, 15).OrderBy(_ => _rnd.Next()).Take(5).ToArray();
+                for (int row = 0; row < 5; row++)
+                    Numbers[row, col] = range[row];
+            }
+        }
+
+        public void Mark(int number)
+        {
+            for (int r = 0; r < 5; r++)
+                for (int c = 0; c < 5; c++)
+                    if (Numbers[r, c] == number)
+                        Marked[r, c] = true;
+        }
+
+        public bool CheckBingo()
+        {
+            // строки
+            for (int r = 0; r < 5; r++)
+            {
+                var row = Enumerable.Range(0, 5).Select(c => Marked[r, c]);
+                if (row.All(m => m)) return true;
+            }
+            // столбцы
+            for (int c = 0; c < 5; c++)
+            {
+                var col = Enumerable.Range(0, 5).Select(r => Marked[r, c]);
+                if (col.All(m => m)) return true;
+            }
+            // диагонали
+            var diag1 = Enumerable.Range(0, 5).Select(i => Marked[i, i]);
+            var diag2 = Enumerable.Range(0, 5).Select(i => Marked[i, 4 - i]);
+            if (diag1.All(m => m) || diag2.All(m => m)) return true;
+
+            return false;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(" B  I  N  G  O");
+            Console.WriteLine(new string('-', 15));
+            for (int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    if (r == 2 && c == 2) Console.Write(" * ");
+                    else Console.Write($"{Numbers[r, c],2} ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
