55b0e18c242d10eca7726250ac2d4ed92a1696c1
a.tyletskaya <a.tyletskaya@evernode.tech>
2026-07-14 17:12:21 +0200

    ...

---
+++ ConsoleApp1/MazeConsole.Tests/MazeModels/Cells/HealthPotionTest.cs
@@ -0,0 +1,36 @@
+﻿using MazeConsole.MazeModels;
+using MazeConsole.MazeModels.Cells;
+using MazeConsole.MazeModels.Intefaces;
+using Moq;
+using NUnit.Framework;
+
+namespace MazeConsole.Tests.MazeModels.Cells
+{
+    internal class HealthPotionTest
+    {
+        [Test] //is cell steppable
+
+        public void PlayerStepInMe_CanStepInHealthPotionCell()
+        {
+            // Preparation
+            var playerMock = new Mock<IPlayer>();  // moq, stub
+            playerMock.SetupAllProperties();
+            var player = playerMock.Object;
+            var mazeMock = new Mock<IMaze>();      // moq, stub
+
+            mazeMock.Setup(maze => maze.LogMessages)
+                .Returns(new List<string>());
+
+            var maze = mazeMock.Object;
+
+            var healthPotion = new HealthPotion();
+            healthPotion.MazeWhereIWasCreated = maze;
+
+            // Act
+            var result = healthPotion.PlayerStepInMe(player);
+
+            // Assert
+            Assert.IsTrue(result, "Health potion is steppable");
+        }
+    }
+}
