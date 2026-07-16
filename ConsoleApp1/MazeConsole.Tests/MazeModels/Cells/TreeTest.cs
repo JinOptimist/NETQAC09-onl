f20aeea2484fcdbf9a6f03d3241009ffee24e0b2
Savicka <Savicka@DESKTOP-7MMIBRJ>
2026-07-16 17:48:43 +0200

    Add the 4th test for Tree

--- ConsoleApp1/MazeConsole.Tests/MazeModels/Cells/TreeTest.cs
+++ ConsoleApp1/MazeConsole.Tests/MazeModels/Cells/TreeTest.cs
@@ -40,11 +40,11 @@ public class TreeTest
     public void PlayerStepInMe_ThrowsNotImplementedExceptionIfGroundIsNotAvailable()
     {
         // Preparation
-        var mazeMock = new Mock<IMaze>();
 
         var playerMock = new Mock<IPlayer>();
         var player = playerMock.Object;
 
+        var mazeMock = new Mock<IMaze>();
         mazeMock.Setup(maze => maze.Cells)
            .Returns(new List<IBaseCell>());
 
@@ -60,5 +60,33 @@ public class TreeTest
         Assert.Throws<NotImplementedException>(() => tree.PlayerStepInMe(player),
             "We expect NotImplementedException when there are no Ground cells ");
 
+    }
+
+    [Test]
+    public void PlayerStepInMe_ReturnsFalseIfGroundIsAvailable()
+    {
+        // Preparation
+        var ground = new Ground();
+        ground.X = 2;
+        ground.Y = 2;
+
+        var mazeMock = new Mock<IMaze>();
+        mazeMock.Setup(maze => maze.Cells)
+           .Returns(new List<IBaseCell> { ground });
+
+        var maze = mazeMock.Object;
+
+        var playerMock = new Mock<IPlayer>();
+        var player = playerMock.Object;
+
+        var tree = new Tree();
+        tree.MazeWhereIWasCreated = maze;
+
+        // Act
+        var result = tree.PlayerStepInMe(player);
+
+
+        // Assert
+        Assert.IsFalse(result, "Its ok when the player can't step on tree");
     }
 }
