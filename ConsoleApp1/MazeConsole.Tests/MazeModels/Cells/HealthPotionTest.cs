a240179566a68347d5801b9983c35279e1f40f77
a.tyletskaya <a.tyletskaya@evernode.tech>
2026-07-16 18:55:30 +0200

    ...

--- ConsoleApp1/MazeConsole.Tests/MazeModels/Cells/HealthPotionTest.cs
+++ ConsoleApp1/MazeConsole.Tests/MazeModels/Cells/HealthPotionTest.cs
@@ -9,32 +9,55 @@ namespace MazeConsole.Tests.MazeModels.Cells
 {
     public class HealthPotionTest
     {
+        private Mock<IPlayer> _playerMock;
+        private Mock<IMaze> _mazeMock;
+        private Mock<Random> _randomMock;
+        private IPlayer _player;
+        private IMaze _maze;
+        private HealthPotion _healthPotion;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Setup Player
+            _playerMock = new Mock<IPlayer>();
+            _playerMock.SetupAllProperties();
+            _player = _playerMock.Object;
+
+            // Setup Maze and Random
+            _mazeMock = new Mock<IMaze>();
+            _randomMock = new Mock<Random>();
+            _randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
+            _mazeMock.Setup(maze => maze.Random)
+                .Returns(_randomMock.Object);
+
+            _maze = _mazeMock.Object;
+
+            // Setup HealthPotion
+            _healthPotion = new HealthPotion();
+            _healthPotion.MazeWhereIWasCreated = _maze;
+        }
+
+        private void SetupMazeCells(params IBaseCell[] cells)
+        {
+            _mazeMock.Setup(maze => maze.Cells)
+                .Returns(new List<IBaseCell>(cells));
+        }
+
+        private void SetupRandomNext(int returnValue)
+        {
+            _randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(returnValue);
+        }
+
         [Test]
         public void PlayerStepInMe_CanStepInHealthPotionCell() //Тест на наступабельность
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>(); // создается мок игрока
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            var mazeMock = new Mock<IMaze>(); //создается мок лабиринта
-
             var groundCell = new Ground { X = 0, Y = 0 }; //в лабиринте размещаем землю, т.к. зелье спавнится только на ней
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { groundCell });
-
-            var randomMock = new Mock<Random>(); //видимо, из-за рандома в HealthPotion, нужно замокать рандом, чтобы тест был предсказуемым
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object; //получаем объект лабиринта из мока
-
-            var healthPotion = new HealthPotion(); //создаем зелье и кладем в мок лабиринта
-            healthPotion.MazeWhereIWasCreated = maze; 
+            SetupMazeCells(groundCell);
 
             // Act
-            var result = healthPotion.PlayerStepInMe(player); // вызываем метод PlayerStepInMe у зелья, передавая в него игрока
+            var result = _healthPotion.PlayerStepInMe(_player); // вызываем метод PlayerStepInMe у зелья, передавая в него игрока
 
             // Assert
             Assert.IsTrue(result, "We expect that player can step on Coin"); // проверяем, что игрок может наступить на зелье
@@ -44,31 +67,14 @@ namespace MazeConsole.Tests.MazeModels.Cells
         public void PlayerStepInMe_HealthPotionReplacedWithGround() //Тест на замену зелья на землю при наступании
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>();
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            var mazeMock = new Mock<IMaze>();
-
             var groundCell = new Ground { X = 0, Y = 0 };
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { groundCell });
-
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object;
-
-            var healthPotion = new HealthPotion();
-            healthPotion.MazeWhereIWasCreated = maze;
+            SetupMazeCells(groundCell);
 
             // Act
-            healthPotion.PlayerStepInMe(player); 
+            _healthPotion.PlayerStepInMe(_player);
 
             // Assert
-            mazeMock.Verify(maze => maze.ReplaceCellToGround(healthPotion), Times.Once,  //проверяем, что метод ReplaceCellToGround был вызван
+            _mazeMock.Verify(maze => maze.ReplaceCellToGround(_healthPotion), Times.Once,  //проверяем, что метод ReplaceCellToGround был вызван
                 "Health potion should be replaced with a ground cell when player steps on it");
         }
 
@@ -76,101 +82,51 @@ namespace MazeConsole.Tests.MazeModels.Cells
         public void PlayerStepInMe_PlayerHealthPotionCountIncrementsCorrectly(int initialHealthPotions, int expectedHealthPotions) //Тест на увеличение количества зелий у игрока
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>(); 
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            player.HealthPotion = initialHealthPotions;
-
-            var mazeMock = new Mock<IMaze>();
+            _player.HealthPotion = initialHealthPotions;
 
             var groundCell = new Ground { X = 0, Y = 0 };
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { groundCell });
-
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object;
-
-            var healthPotion = new HealthPotion();
-            healthPotion.MazeWhereIWasCreated = maze;
+            SetupMazeCells(groundCell);
 
             // Act
-            healthPotion.PlayerStepInMe(player);
+            _healthPotion.PlayerStepInMe(_player);
 
             // Assert
-            Assert.AreEqual(expectedHealthPotions, player.HealthPotion, // проверяем, что количество зелий у игрока увеличилось на 1
+            Assert.AreEqual(expectedHealthPotions, _player.HealthPotion, // проверяем, что количество зелий у игрока увеличилось на 1
                 $"Player should have {expectedHealthPotions} health potions after stepping on the health potion cell");
         }
 
         [TestCase(1)]
         [TestCase(2)]
-        public void PlayerStepInMe_ThrowsExceptionWhenPlayerAlreadyHasMaxHealthPotions(int initialHealthPotions) //Тест на эксепшн, когда у игрока уже есть максимум зелий
+        public void PlayerStepInMe_ThrowsExceptionWhenPlayerAlreadyHasMaxHealthPotions(int initialHealthPotions) //Тест на экзепшн, когда у игрока уже есть максимум зелий
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>(); 
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            player.HealthPotion = initialHealthPotions;
-
-            var mazeMock = new Mock<IMaze>();
+            _player.HealthPotion = initialHealthPotions;
 
             var groundCell = new Ground { X = 0, Y = 0 };
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { groundCell });
-
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object;
-
-            var healthPotion = new HealthPotion();
-            healthPotion.MazeWhereIWasCreated = maze;
+            SetupMazeCells(groundCell);
 
             // Act & Assert
-            Assert.Throws<InvalidOperationException>(() => healthPotion.PlayerStepInMe(player)); //проверяем, что есть эксепшн
+            Assert.Throws<InvalidOperationException>(() => _healthPotion.PlayerStepInMe(_player)); //проверяем, что есть экзепшн
         }
 
         [Test]
-        public void PlayerStepInMe_NewHealthPotionReplacedToRandomGround() //Тест на создание нового зелья с координатами случайной земли
+        public void PlayerStepInMe_NewHealthPotionReplacedToRandomGround() //Тест на создание нового зелья с координатами слуйчайной земли
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>(); 
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            var mazeMock = new Mock<IMaze>();
-
             var groundCell1 = new Ground { X = 2, Y = 3 }; //создаем первую ячейку земли 
             var groundCell2 = new Ground { X = 5, Y = 7 }; //создаем вторую ячейку земли (куда упадет новое зелье)
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { groundCell1, groundCell2 });
+            SetupMazeCells(groundCell1, groundCell2);
+            SetupRandomNext(1); //кладем в groundCell2
 
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(1); //кладем в groundCell2
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object;
-
-            var healthPotion = new HealthPotion();
-            healthPotion.MazeWhereIWasCreated = maze;
-
-            IBaseCell capturedCell = null; //Cоздаем коробочку для сохранения нового зелья
-            mazeMock.Setup(m => m.ReplaceToCell(It.IsAny<IBaseCell>())) //настраиваем перехват вызова ReplaceToCell. Когда  в тесте будет вызван ReplaceToCell, то мок перехватит это зелье
+            IBaseCell capturedCell = null; //Созадем коробочку для сохранения нового зелья
+            _mazeMock.Setup(m => m.ReplaceToCell(It.IsAny<IBaseCell>())) //настраиваем перехват вызова ReplaceToCell. Когда  в тесте будет вызван ReplaceToCell, то мок перехватит это зелье
                 .Callback<IBaseCell>(cell => capturedCell = cell); //а с помощью коллбека сохранит это зелье в переменную capturedCell, чтобы потом проверить его наличие и координаты
 
             // Act
-            healthPotion.PlayerStepInMe(player);
+            _healthPotion.PlayerStepInMe(_player);
 
             // Assert 
-            mazeMock.Verify(maze => maze.ReplaceToCell(It.IsAny<IBaseCell>()), Times.Once, //проверяем, что ReplaceToCell был вызван
+            _mazeMock.Verify(maze => maze.ReplaceToCell(It.IsAny<IBaseCell>()), Times.Once, //проверяем, что ReplaceToCell был вызван
                 "ReplaceToCell should be called once");
 
             Assert.That(capturedCell, Is.Not.Null, "ReplaceToCell should have been called with a cell"); //проверяем, что зелье было передано
@@ -185,28 +141,11 @@ namespace MazeConsole.Tests.MazeModels.Cells
         public void HealthPotion_ThrowsExceptionWhenNoGround()
         {
             // Preparation
-            var playerMock = new Mock<IPlayer>();
-            playerMock.SetupAllProperties();
-            var player = playerMock.Object;
-
-            var mazeMock = new Mock<IMaze>();
-
             var wallCell = new Wall { X = 0, Y = 0 }; //добавляем только клетку стены, чтобы проверить, что зелье не может быть создано
-            mazeMock.Setup(maze => maze.Cells)
-                .Returns(new List<IBaseCell> { wallCell });
-
-            var randomMock = new Mock<Random>();
-            randomMock.Setup(r => r.Next(It.IsAny<int>())).Returns(0);
-            mazeMock.Setup(maze => maze.Random)
-                .Returns(randomMock.Object);
-
-            var maze = mazeMock.Object;
-
-            var healthPotion = new HealthPotion();
-            healthPotion.MazeWhereIWasCreated = maze;
+            SetupMazeCells(wallCell);
 
             // Act & Assert
-            Assert.Throws<InvalidOperationException>(() => healthPotion.PlayerStepInMe(player));
+            Assert.Throws<InvalidOperationException>(() => _healthPotion.PlayerStepInMe(_player));
         }
     }
 }
