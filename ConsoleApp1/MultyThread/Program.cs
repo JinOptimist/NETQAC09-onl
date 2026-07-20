ef374423f94f7b5f6a728ff4f8b95e49b4ce3ffd
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-07-20 21:45:05 +0300

    Add example for async and multy thread

---
+++ ConsoleApp1/MultyThread/Program.cs
@@ -0,0 +1,46 @@
+﻿// Поток данных
+// Stream
+
+// поток выполнения
+// Thread
+
+using MultyThread;
+using System.Net.Http.Headers;
+
+var reportGenerator = new ReportGenerator();
+reportGenerator.GenerateAllRepots();
+
+
+var tasks = new List<Task>();
+
+for (int i = 0; i < 100000; i++)
+{
+    var task = new Task(DoMagic);
+    tasks.Add(task);
+}
+
+tasks.ForEach(x => x.Start());
+
+Task.WaitAll(tasks);
+
+Console.ReadLine();
+
+void DoMagic()
+{
+    var counter = 0;
+    while (true)
+    {
+        counter++;
+        Console.WriteLine("AAAAA "  + counter);
+    }
+}
+
+void DoMagicB()
+{
+    var counter = 0;
+    while (true)
+    {
+        counter++;
+        Console.WriteLine("BB " + counter);
+    }
+}
