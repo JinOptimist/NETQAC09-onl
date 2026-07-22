295f51c63c49d048512fe6d768c2d8d61692b0b1
a.tyletskaya <a.tyletskaya@evernode.tech>
2026-07-22 16:32:57 +0200

    ...

--- ConsoleApp1/WebAppSmile/Services/IceApiDataService.cs
+++ ConsoleApp1/WebAppSmile/Services/IceApiDataService.cs
@@ -1,8 +1,8 @@
 namespace WebAppSmile.Services;
 
-public class IceApiDataService
+public class IceApiDataService 
 {
-    public async Task<T> GetDataFromApiAsync<T>(string url)
+    public async Task<T> GetDataFromApiAsync<T>(string url) // универсальный метод для получения данных с API
     {
         var http = new HttpClient();
         var response = await http.GetAsync(url);
